f4269568adc7f9846b61d6a7ee2ca720d6e6eb13
Koramit Pichanaharee <koramit.p@gmail.com>
2021-09-22 15:20:37 +0700

    add policies

--- app/Policies/OPDCardPolicy.php
+++ app/Policies/OPDCardPolicy.php
@@ -20,8 +20,40 @@ class OPDCardPolicy
         //
     }
 
+    // 'triage_case',
+    public function triage(User $user, OPDCard $opdcard)
+    {
+        return $user->abilities->contains('triage_case')
+            && ! $opdcard->triage;
+    }
+
+    // 'exam_case',
     public function exam(User $user, OPDCard $opdcard)
     {
-        return $opdcard->triage;
+        return $user->abilities->contains('exam_case')
+            && $opdcard->triage
+            && ! $opdcard->exam_completed_at;
+    }
+
+    // 'procedure_case',
+    public function procedure(User $user, OPDCard $opdcard)
+    {
+        return $user->abilities->contains('procedure_case')
+            && $opdcard->triage
+            && ! $opdcard->exam_completed_at;
+    }
+
+    // 'discharge_case',
+    public function discharge(User $user, OPDCard $opdcard)
+    {
+        return $user->abilities->contains('discharge_case')
+            && $opdcard->exam_completed_at;
+    }
+
+    // 'cancel_case',
+    public function cancel(User $user, OPDCard $opdcard)
+    {
+        return $user->abilities->contains('cancel_case')
+            && ! $opdcard->exam_completed_at;
     }
 }
